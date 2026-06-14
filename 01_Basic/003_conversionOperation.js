3a026dd2de3b84d6c5a54500530c67d15783d76d
Bankal-2708 <bankalmavi1@gmail.com>
2026-06-14 12:54:40 +0530

    Conversional Operations

---
+++ 01_Basic/003_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "34q";
+let name = "Bankal";
+let NumScore = Number(score);
+
+// console.log(typeof score);
+// console.log(NumScore) // NaN 
+// console.log(Number(name)); // NaN
+
+
+let isLoggedIn = true;
+let LoggedIn = 1;
+let nullLoggedIn = null;
+let stringLoggedIn = "";
+let strLoggedIn = "bankal";
+
+
+console.log(Number(isLoggedIn)) // 1
+console.log(Boolean(LoggedIn)); // true
+console.log(Boolean(nullLoggedIn)); // false
+console.log(Boolean(stringLoggedIn)) // false
+console.log(Boolean(strLoggedIn)); // true
