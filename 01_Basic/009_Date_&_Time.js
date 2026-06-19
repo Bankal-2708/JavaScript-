c6096065fa587960e03f3bf40bd7d7ec786ad4a1
Bankal-2708 <bankalmavi1@gmail.com>
2026-06-19 21:49:15 +0530

    Date and Time

---
+++ 01_Basic/009_Date_&_Time.js
@@ -0,0 +1,11 @@
+// Date 
+
+const myDate = new Date();
+console.log(myDate)
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+
+
+console.log(typeof myDate) // object
