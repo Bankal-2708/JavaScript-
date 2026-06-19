4e487fa9a62a385c2968b6a78e849aa04af471fc
Bankal-2708 <bankalmavi1@gmail.com>
2026-06-19 23:58:42 +0530

    Update Date and Time

--- 01_Basic/009_Date_&_Time.js
+++ 01_Basic/009_Date_&_Time.js
@@ -1,12 +1,26 @@
 // Date 
 
 const myDate = new Date();
-console.log(myDate)
-console.log(myDate.toDateString())
-console.log(myDate.toISOString())
-console.log(myDate.toLocaleDateString())
-console.log(myDate.toLocaleString())
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
 
 
-console.log(typeof myDate) // object
+// console.log(typeof myDate) // object
+// const bookingDate = new Date("06-15-2026")
+
+// const currentDate = Date.now()
+// console.log(currentDate.toLocaleString())
+// console.log(bookingDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+const todayDate = new Date();
+
+console.log(
+  todayDate.toLocaleDateString('default', {
+    weekday: 'long'
+  })
+);
 
