1a5d545aad35b2aefce2c743162745a6377b4bdd
Bankal-2708 <bankalmavi1@gmail.com>
2026-06-25 12:13:54 +0530

    branch added

---
+++ 03_Basic_Function/004_IIFE.js
@@ -0,0 +1,15 @@
+// immediate invoked function expression : executed immediately
+// why : we face problem sometime due to pollution of global scope , we use this to remove tha variable or pollution of global 
+
+
+(function(){
+
+  console.log("DB Connected");
+  
+})();
+
+((name)=>{
+  console.log(`DB CONNECTED SECOND ${name}`);
+  
+})('Bankal Mavi');
+
