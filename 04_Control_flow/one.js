e28ab155bce21a7960afe34af3f5fe8fa7bc3da2
Bankal-2708 <bankalmavi1@gmail.com>
2026-06-26 17:04:47 +0530

    truthy and false values

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,80 @@
+// const isLoggedIn = false
+
+// if(isLoggedIn){
+//   console.log("User logged in.");
+// } else {
+//   console.log("User not logged in.");
+// }
+
+
+// const balance = 100;
+
+// if(balance < 500){
+//   console.log("balancs is less than 500");
+// } else if (balance < 750){
+//   console.log("balance is less than 750")
+// }else {
+//   console.log("balance is less than 1200")
+// }
+
+
+// switch case 
+
+ 
+
+const month = 10;
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+
+  case 2:
+    console.log("February");
+    break;
+
+  case 3:
+    console.log("March");
+    break;
+
+  case 4:
+    console.log("April");
+    break;
+
+  case 5:
+    console.log("May");
+    break;
+
+  case 6:
+    console.log("June");
+    break;
+
+  case 7:
+    console.log("July");
+    break;
+
+  case 8:
+    console.log("August");
+    break;
+
+  case 9:
+    console.log("September");
+    break;
+
+  case 10:
+    console.log("October");
+    break;
+
+  case 11:
+    console.log("November");
+    break;
+
+  case 12:
+    console.log("December");
+    break;
+
+  default:
+    console.log("Invalid month");
+}
+
+  
